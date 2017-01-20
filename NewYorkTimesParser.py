dfe5f2f9355a722b6721f7fc36fcc2d9a434a94d
Alex Niznik <alex@niznik.info>
2017-01-19 20:33:59 -0600

    parsing only main content now

--- NewYorkTimesParser.py
+++ NewYorkTimesParser.py
@@ -7,22 +7,25 @@ r = requests.get(url)
 r_html = r.text
 
 soup = BeautifulSoup(r_html, "html.parser")
-headings = soup.find_all('h2', attrs={"class": "story-heading"})
+divTag = soup.find_all("div", class_="page")
+
 
 
 def print_Info():
     num = 0
 
-    for title in headings:
-        for string in title.stripped_strings:
-            num += 1
+    for heading in divTag:
+        headings = heading.find_all('h2', attrs={"class": "story-heading"})
+        for title in headings:
+            for string in title.stripped_strings:
+                num += 1
 
-    print(datetime.date.today())
-    print("There are " + str(num) + " articles on The New York Times today.\n\n")
+        print(datetime.date.today())
+        print("There are " + str(num) + " articles on the main page of The New York Times today.\n\n")
 
-    for title in headings:
-        for string in title.stripped_strings:
-            print (string)
+        for title in headings:
+            for string in title.stripped_strings:
+                print (string)
 
 
 print_Info()
