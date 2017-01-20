38febc3833c16398ad230daaf907ddbe9327265f
Alex Niznik <alex@niznik.info>
2017-01-19 20:45:52 -0600

    structured the code better

--- NewYorkTimesParser.py
+++ NewYorkTimesParser.py
@@ -2,19 +2,27 @@ import requests
 import datetime 
 from bs4 import BeautifulSoup
 
-url = 'https://www.nytimes.com/'
-r = requests.get(url)
-r_html = r.text
 
-soup = BeautifulSoup(r_html, "html.parser")
-divTag = soup.find_all("div", class_="page")
+def setup():
+    url = 'https://www.nytimes.com/'
+    r = requests.get(url)
+    r_html = r.text
+
+    soup = BeautifulSoup(r_html, "html.parser")
+    divtag = soup.find_all("div", class_="page")
+
+    return divtag
 
 
 
-def print_Info():
+def print_info():
+    """
+    Prints date, number of articles and all their titles
+    """
     num = 0
 
-    for heading in divTag:
+    divtag = setup()
+    for heading in divtag:
         headings = heading.find_all('h2', attrs={"class": "story-heading"})
         for title in headings:
             for string in title.stripped_strings:
@@ -25,7 +33,6 @@ def print_Info():
 
         for title in headings:
             for string in title.stripped_strings:
-                print (string)
+                print(string)
 
-
-print_Info()
+print_info()
